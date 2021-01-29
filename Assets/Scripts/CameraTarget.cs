b5b7645245900122ecebf0542d0c15c4d2de83ac
Barad-dur\Bergur <bergura@hotmail.com>
2021-01-29 14:58:56 +0000

    init - base project from canvas
    Setting up project.

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace AGDDPlatformer
+{
+    public class CameraTarget : MonoBehaviour
+    {
+        public Transform[] targets;
+
+        void LateUpdate()
+        {
+            float averageX = 0;
+            foreach (Transform target in targets)
+            {
+                averageX += target.position.x;
+            }
+
+            averageX /= targets.Length;
+
+            transform.position = new Vector3(averageX, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AGDDPlatformer
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        [Header("Players")]
+        public PlayerController[] players;
+
+        [Header("Level")]
+        public PlayerGoal[] playerGoals;
+        public bool timeStopped;
+        public bool isGameComplete;
+        public string firstLevel;
+        public string nextLevel;
+
+        [Header("Level Transition")]
+        public GameObject startScreen;
+        public GameObject endScreen;
+        public GameObject gameOverScreen;
+        public float startScreenTime = 1.0f;
+        public float endScreenDelay = 1.0f;
+        public float endScreenTime = 1.0f;
+
+        [Header("Audio")]
+        public AudioSource source;
+        public AudioClip winSound;
+
+        void Awake()
+        {
+            instance = this;
+
+            if (playerGoals.Length == 0)
+            {
+                playerGoals = FindObjectsOfType<PlayerGoal>();
+            }
+        }
+
+        IEnumerator Start()
+        {
+            timeStopped = true;
+
+            endScreen.SetActive(false);
+            gameOverScreen.SetActive(false);
+
+            startScreen.SetActive(true);
+
+            yield return new WaitForSeconds(startScreenTime);
+
+            startScreen.SetActive(false);
+
+            timeStopped = false;
+        }
+
+        void Update()
+        {
+            if (isGameComplete)
+            {
+                if (Input.GetButtonDown("Reset"))
+                {
+                    ResetGame();
+                }
+            }
+
+            if (timeStopped)
+                return;
+
+            /* --- Check Player Goals --- */
+
+            bool allGoalsSatisfied = true;
+            foreach (PlayerGoal playerGoal in playerGoals)
+            {
+                if (!playerGoal.isSatisfied)
+                {
+                    allGoalsSatisfied = false;
+                    break;
+                }
+            }
+
+            if (allGoalsSatisfied)
+            {
+                source.PlayOneShot(winSound);
+                StartCoroutine(LevelCompleted());
+            }
+
+            if (Input.GetButtonDown("Reset"))
+            {
+                ResetLevel();
+            }
+        }
+
+        IEnumerator LevelCompleted()
+        {
+            timeStopped = true;
+
+            yield return new WaitForSeconds(endScreenDelay);
+
+            endScreen.SetActive(true);
+
+            yield return new WaitForSeconds(endScreenTime);
+
+            if (!string.IsNullOrEmpty(nextLevel))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+            else
+            {
+                isGameComplete = true;
+                gameOverScreen.SetActive(true);
+            }
+        }
+
+        void ResetGame()
+        {
+            SceneManager.LoadScene(firstLevel);
+        }
+
+        void ResetLevel()
+        {
+            foreach (PlayerController player in players)
+            {
+                player.ResetPlayer();
+            }
+        }
+    }
+}
