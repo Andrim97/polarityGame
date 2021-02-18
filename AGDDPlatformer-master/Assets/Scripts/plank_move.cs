8be6a575c936bacc2d58c3c38d25b53beac6b851
ThorgeirBjornsson <Thorgeir18@ru.is>
2021-02-18 10:50:23 +0000

    movable plank script added

--- AGDDPlatformer-master/Assets/Scripts/plank_move.cs
+++ AGDDPlatformer-master/Assets/Scripts/plank_move.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class plank_move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+               
     }
 }
