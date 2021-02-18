cf6dbe8e595ebadfaea1245cd16d919fb5fe20d9
ThorgeirBjornsson <Thorgeir18@ru.is>
2021-02-18 12:21:44 +0000

    movable plank dont working

--- AGDDPlatformer-master/Assets/Scripts/plank_move.cs
+++ AGDDPlatformer-master/Assets/Scripts/plank_move.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class plank_move : MonoBehaviour
 {
-
+    
+    public float speed = 1.5f;
+    
     // Update is called once per frame
     void Update()
     {
-               
+        float step = speed * Time.deltaTime;
+        transform.Translate(step, 0, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("this is anotuher place");
     }
 }
