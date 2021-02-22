c4dbff0429c35a1a1e1098bd2a6b23fb7a467be5
Andrim97 <42970336+Andrim97@users.noreply.github.com>
2021-02-22 14:50:05 +0000

    moveable plank partly fixed

--- AGDDPlatformer-master/Assets/Scripts/plank_move.cs
+++ AGDDPlatformer-master/Assets/Scripts/plank_move.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,25 @@ public class plank_move : MonoBehaviour
 {
     
     public float speed = 1.5f;
+    float step;
+
+    float dir;
+    void Start()
+    {
+        step = speed * Time.deltaTime;
+        dir = step;
+    }
     
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        transform.Translate(step, 0, 0);
+        if(transform.position.x > 5) {
+            dir = -step;
+        }
+        if(transform.position.x < 1) {
+            dir = step;
+        }
+        transform.Translate(dir, 0, 0);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
